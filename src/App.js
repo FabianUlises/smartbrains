6852bd5aa65e12002165446d2c1c4c1ac9313139
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-05 12:40:18 -0800

    Running into a problem using particles library

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 // components
 import Navigation from './components/Navigation/Navigation';
@@ -5,9 +6,22 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 // Libraries
 import 'tachyons';
+// import Particles from 'react-particles-js';
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 function App() {
   return (
     <div className="App">
+      {/* <Particles params={particlesOptions} className='particles'/> */}
       <Navigation />
       <Rank />
       <ImageLinkForm />
