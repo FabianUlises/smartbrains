7b9ed8a457721c3c77bc460041d5b23050f89a3d
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-05 12:23:42 -0800

    Created a rank component. I am also adding styles using an npm package

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 // components
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 // Libraries
 import 'tachyons';
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import Logo from '../Logo/Logo';
 
 const Navigation = () => {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'space-between', marginRight: '.5rem' }}>
+    <nav style={{ display: 'flex', justifyContent: 'space-between', marginRight: '.5rem', paddingBlockStart: '1rem' }}>
         <Logo />
         <p className='f3 link dim underline pa3 pointer'>sign out</p>
     </nav>
