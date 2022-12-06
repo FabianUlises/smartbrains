4099389963841e0d53074f2dd75826c1058cfcb3
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-06 14:09:20 -0800

    Fixed some syntax errors devtools was showing

--- src/components/FaceRecognition/Facerecognition.js
+++ src/components/FaceRecognition/Facerecognition.js
@@ -5,7 +5,7 @@ const Facerecognition = ({ imageUrl, box }) => {
   return (
     <div>
       <div className="absolute mt2 image-container">
-        <img id='input-image' src={imageUrl} width='500px' height='auto'/>
+        <img id='input-image' src={imageUrl} width='500px' height='auto' alt='random face for face detection'/>
         <div className="bounding-box" style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
       </div>
     </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,9 @@ const Logo = () => {
   return (
     <div className='ma4 mt0'>
         <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 100, width: 100 }} >
-        <div className="Tilt-inner">👽</div>
+        <div className="Tilt-inner">
+          <span role='img' aria-label='alien icon'>👽</span>
+        </div>
         </Tilt>
     </div>
   )
