94cf255079ff487b97ee82858a042506da4329b6
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-06 13:13:45 -0800

    Created register form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,33 +1,34 @@
 import React from 'react';
-import './signin.css';
+import './register.css';
 
-const Signin = ({ routeChange }) => {
+const Register = ({ routeChange }) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw5 shadow-5 center form-article">
         <main className="pa4">
             <form className="measure">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+                    <legend className="f4 fw6 ph0 mh0">Register</legend>
+                    <div className="mt3">
+                        <label className="db fw6 lh-copy f6" for="fullName">Name</label>
+                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="fullName"  id="fullName" />
+                    </div>
                     <div className="mt3">
                         <label className="db fw6 lh-copy f6" for="email-address">Email</label>
                         <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
-                    </div>
+                    </div> 
                     <div className="mv3">
                         <label className="db fw6 lh-copy f6" for="password">Password</label>
                         <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
                     </div>
                 </fieldset>
                 <div className="reset-btn">
-                    <input onClick={() => routeChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+                    <input onClick={() => routeChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign up" />
                     {/* <button onclick={() => console.log(route)}>sign in</button> */}
                 </div>
-                <div className="lh-copy mt3 signup-btn">
-                    <p onClick={() => routeChange('register')} className="f6 link dim db pointer">Register</p>
-                </div>
-            </form> 
+            </form>
         </main>
     </article>
     )
 }
 
-export default Signin;
+export default Register;
